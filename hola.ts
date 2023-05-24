3751971803fdc71326016430fdeda9947b14f128
NoemiMatiasReynoso <noemimatias384@gmail.com>
2023-05-24 14:19:39 +0200

    33

--- hola.ts
+++ hola.ts
@@ -3,9 +3,4 @@ console.log("visuslcode")
 console.log("hola mundo111!")
 console.log("este archivo sera un conflicto")
 console.log("segundo conflicto desde git")
-<<<<<<< HEAD
 console.log("tercer conflicto dessde visual")
-=======
-console.log("tercer conflcto")
-
->>>>>>> 1944cce2f5a52913735d5cab8721a416d9e66075
